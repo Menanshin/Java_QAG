a3da9bfe3a9b2157eafcede13bae53490b832a23
e.maslov <e.maslov@vk.team>
2023-01-23 00:05:34 +0300

    change int  i=7

--- src/test/java/HM_QAG_2.java
+++ src/test/java/HM_QAG_2.java
@@ -4,7 +4,7 @@ public class HM_QAG_2 {
 
     @Test
     void newTest() {
-        int i = 5;
+        int i = 7;
         System.out.println("This is the test");
         Assertions.assertTrue(i > 2);
     }
