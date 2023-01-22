2c99363238c1e386b27cf2cc6a98b05d91b0316b
e.maslov <e.maslov@vk.team>
2023-01-22 23:49:32 +0300

    change int  i=4

---
+++ src/test/java/HM_QAG_2.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class HM_QAG_2 {
+
+    @Test
+    void newTest() {
+        int i = 4;
+        System.out.println("This is the test");
+        Assertions.assertTrue(i > 2);
+    }
+}
