1e82038a15f1c6ddf412edd678ece9f5963f5007
e.maslov <e.maslov@vk.team>
2023-01-24 18:35:35 +0300

    add Home work part 1

---
+++ src/test/java/HM_QAG_3.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+
+public class HM_QAG_3 {
+    @Test
+    void checkGitHubPage() {
+        Configuration.holdBrowserOpen = true;
+        //открываем главную страницу GitHub
+        open("https://github.com/");
+        //наводим курсор на надпись Solutions
+        $(byText("Solutions")).hover();
+        //выбираем пункт Enterprise
+        $(byText("Enterprise")).click();
+        //проверяем, что открылась нужная страница
+        $(".font-mktg").shouldHave(text("Build like the best"));
+    }
+}
