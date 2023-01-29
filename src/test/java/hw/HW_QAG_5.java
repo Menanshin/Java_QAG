0276aaccde1bfc97e6d354e418ed563d913594eb
e.maslov <e.maslov@vk.team>
2023-01-30 01:28:53 +0300

    add Home work for java _1

---
+++ src/test/java/hw/HW_QAG_5.java
@@ -0,0 +1,10 @@
+package hw;
+//задание на переполнение
+public class HW_QAG_5 {
+    public static void main() {
+        byte x = 127;
+        byte y = 127;
+        byte z = x + y;
+        System.out.println(z);
+    }
+}
---
+++ src/test/java/hw/HW_QAG_5_1.java
@@ -0,0 +1,12 @@
+package hw;
+//задание на пкоминирование типов
+public class HW_QAG_5_1 {
+    public static void intAndDouble() {
+        int a = 120;
+        double b = 12.5;
+        int i = (a + b);
+        double d = (a + b);
+        System.out.println(i);
+        System.out.println(d);
+    }
+}
